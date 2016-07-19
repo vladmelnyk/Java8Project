9e50ce5dabb09ecf45daff6d32db66af13f06c2c
Denys <ievstratenko.denys@gmail.com>
2016-07-19 11:33:20 +0300

    Functions with method ref

--- src/Runner.java
+++ src/Runner.java
@@ -6,18 +6,8 @@ public class Runner {
     public static void main(String[] args) {
         String[] words = {"Test", "lol", "abcde", "XYZ", "123"};
         boolean lower = false;
-        Function<String, String> toLowerCase = new Function<String, String>() {
-            @Override
-            public String apply(String s) {
-                return s.toLowerCase();
-            }
-        };
-        Function<String, String> toUpperCase = new Function<String, String>() {
-            @Override
-            public String apply(String s) {
-                return s.toUpperCase();
-            }
-        };
+        Function<String, String> toLowerCase = String::toLowerCase;
+        Function<String, String> toUpperCase = s -> s.toUpperCase();
         Function<String, String> f = lower ? toLowerCase : toUpperCase;
         for (int i = 0; i < words.length; i++) {
             words[i] = f.apply(words[i]);
