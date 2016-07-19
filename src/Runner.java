1866e4613016426b2d9778f21d0a4374757dff28
Denys <ievstratenko.denys@gmail.com>
2016-07-19 11:30:39 +0300

    Anonymous classes

--- src/Runner.java
+++ src/Runner.java
@@ -1,10 +1,27 @@
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.function.Function;
 
 public class Runner {
     public static void main(String[] args) {
-        String[] words = {"test", "lol", "abcde", "XYZ", "123"};
-        Arrays.sort(words, (o1, o2) -> o1.length() - o2.length());
+        String[] words = {"Test", "lol", "abcde", "XYZ", "123"};
+        boolean lower = false;
+        Function<String, String> toLowerCase = new Function<String, String>() {
+            @Override
+            public String apply(String s) {
+                return s.toLowerCase();
+            }
+        };
+        Function<String, String> toUpperCase = new Function<String, String>() {
+            @Override
+            public String apply(String s) {
+                return s.toUpperCase();
+            }
+        };
+        Function<String, String> f = lower ? toLowerCase : toUpperCase;
+        for (int i = 0; i < words.length; i++) {
+            words[i] = f.apply(words[i]);
+        }
         System.out.println(Arrays.toString(words));
 
     }
