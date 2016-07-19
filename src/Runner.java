c470ce925b68d9c0e49d669ee3eae945a56cb1b5
Denys <ievstratenko.denys@gmail.com>
2016-07-19 10:48:33 +0300

    Comparator with lambdas

--- src/Runner.java
+++ src/Runner.java
@@ -1,6 +1,11 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class Runner {
     public static void main(String[] args) {
-        Runnable r = () -> System.out.println("hello111");
-        r.run();
+        String[] words = {"test", "lol", "abcde", "XYZ", "123"};
+        Arrays.sort(words, (o1, o2) -> o1.length() - o2.length());
+        System.out.println(Arrays.toString(words));
+
     }
 }
