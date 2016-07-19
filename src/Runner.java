e0c4c7389239acbe12338e32629a366afecd2f6a
Denys <ievstratenko.denys@gmail.com>
2016-07-19 11:48:03 +0300

    Transitions from anonymous to method ref

--- src/Runner.java
+++ src/Runner.java
@@ -1,14 +1,28 @@
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.function.Function;
+import java.util.function.UnaryOperator;
 
 public class Runner {
     public static void main(String[] args) {
         String[] words = {"Test", "lol", "abcde", "XYZ", "123"};
         boolean lower = false;
-        Function<String, String> toLowerCase = String::toLowerCase;
-        Function<String, String> toUpperCase = s -> s.toUpperCase();
-        Function<String, String> f = lower ? toLowerCase : toUpperCase;
+        UnaryOperator<String> toLowerCase1 = new UnaryOperator<String>() {
+            @Override
+            public String apply(String s) {
+                return s.toLowerCase();
+            }
+        };
+        UnaryOperator<String> toLowerCase2 = (String s) -> {
+            return s.toLowerCase();
+        };
+        UnaryOperator<String> toLowerCase3 = (String s) -> s.toLowerCase();
+        UnaryOperator<String> toLowerCase4 = (s) -> s.toLowerCase();
+        UnaryOperator<String> toLowerCase5 = s -> s.toLowerCase();
+        UnaryOperator<String> toLowerCase6 = String::toLowerCase;
+
+        UnaryOperator<String> toLowerCase = String::toLowerCase;
+        UnaryOperator<String> toUpperCase = s -> s.toUpperCase();
+        UnaryOperator<String> f = lower ? toLowerCase : toUpperCase;
         for (int i = 0; i < words.length; i++) {
             words[i] = f.apply(words[i]);
         }
